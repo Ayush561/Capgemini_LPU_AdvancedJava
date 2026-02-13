97036d758524a62b33b5ea7eff55e4723a5c0b2c
Ayush561 <ayushans56@gmail.com>
2026-02-13 22:56:42 +0530

    Added to existing

---
+++ PassPortHibernate/src/test/java/Passport_Test.java
@@ -0,0 +1,70 @@
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import Passport_Hibernate_Assignment.Passport_Driver;
+
+public class Passport_Test {
+
+    static EntityManagerFactory emf;
+    static EntityManager em;
+    static EntityTransaction et;
+
+    @BeforeAll
+    public static void setup() {
+        emf = Persistence.createEntityManagerFactory("postgres");
+        em = emf.createEntityManager();
+        et = em.getTransaction();
+    }
+
+    @Test
+    public void testInsertData() {
+        Passport_Driver.insertData();
+    }
+
+    @Test
+    public void testFindDataById() {
+        boolean b = Passport_Driver.findDataById(101);
+        assertTrue(b);
+    }
+
+    @Test
+    public void testNullFindDataById() {
+        boolean b = Passport_Driver.findDataById(999);
+        assertFalse(b);
+    }
+
+    @Test
+    public void testUpdateDataById() {
+        boolean b = Passport_Driver.updateDataById(101, "Amit", 12);
+        assertTrue(b);
+    }
+
+    @Test
+    public void testNullUpdateDataById() {
+        boolean b = Passport_Driver.updateDataById(999, "None", 0);
+        assertFalse(b);
+    }
+
+    @Test
+    public void testDeleteDataById() {
+        Passport_Driver driver = new Passport_Driver();
+        boolean b = driver.deleteDataById(101);
+        assertTrue(b);
+    }
+
+    @Test
+    public void testNullDeleteDataById() {
+        Passport_Driver driver = new Passport_Driver();
+        boolean b = driver.deleteDataById(999);
+        assertFalse(b);
+    }
+}
